731e94c5ad8805116061b4376e4a9aaa84bafce4
zty98 <zhang-ty17@mails.tsinghua.edu.cn>
2020-09-24 16:21:11 +0800

    mkdir for codah

---
+++ scripts/mkdir_codah.sh
@@ -0,0 +1,50 @@
+# create output folders for dream
+mkdir -p data/codah/fold_0/fairseq/
+mkdir -p data/codah/fold_0/fairseq/official/
+mkdir -p data/codah/fold_0/fairseq/inhouse/
+mkdir -p data/codah/fold_0/grounded/
+mkdir -p data/codah/fold_0/paths/
+mkdir -p data/codah/fold_0/graph/
+mkdir -p data/codah/fold_0/statement/
+mkdir -p data/codah/fold_0/tokenized/
+mkdir -p data/codah/fold_0/roberta/
+
+mkdir -p data/codah/fold_1/fairseq/
+mkdir -p data/codah/fold_1/fairseq/official/
+mkdir -p data/codah/fold_1/fairseq/inhouse/
+mkdir -p data/codah/fold_1/grounded/
+mkdir -p data/codah/fold_1/paths/
+mkdir -p data/codah/fold_1/graph/
+mkdir -p data/codah/fold_1/statement/
+mkdir -p data/codah/fold_1/tokenized/
+mkdir -p data/codah/fold_1/roberta/
+
+mkdir -p data/codah/fold_2/fairseq/
+mkdir -p data/codah/fold_2/fairseq/official/
+mkdir -p data/codah/fold_2/fairseq/inhouse/
+mkdir -p data/codah/fold_2/grounded/
+mkdir -p data/codah/fold_2/paths/
+mkdir -p data/codah/fold_2/graph/
+mkdir -p data/codah/fold_2/statement/
+mkdir -p data/codah/fold_2/tokenized/
+mkdir -p data/codah/fold_2/roberta/
+
+mkdir -p data/codah/fold_3/fairseq/
+mkdir -p data/codah/fold_3/fairseq/official/
+mkdir -p data/codah/fold_3/fairseq/inhouse/
+mkdir -p data/codah/fold_3/grounded/
+mkdir -p data/codah/fold_3/paths/
+mkdir -p data/codah/fold_3/graph/
+mkdir -p data/codah/fold_3/statement/
+mkdir -p data/codah/fold_3/tokenized/
+mkdir -p data/codah/fold_3/roberta/
+
+mkdir -p data/codah/fold_4/fairseq/
+mkdir -p data/codah/fold_4/fairseq/official/
+mkdir -p data/codah/fold_4/fairseq/inhouse/
+mkdir -p data/codah/fold_4/grounded/
+mkdir -p data/codah/fold_4/paths/
+mkdir -p data/codah/fold_4/graph/
+mkdir -p data/codah/fold_4/statement/
+mkdir -p data/codah/fold_4/tokenized/
+mkdir -p data/codah/fold_4/roberta/
