1dbc682823a522b732e318024425937e79673ee2
zty98 <zhang-ty17@mails.tsinghua.edu.cn>
2020-09-24 15:58:49 +0800

    preprocess of CODAH Datasets

--- utils/convert_codah.py
+++ utils/convert_codah.py
@@ -18,29 +18,33 @@ def split(full_list, shuffle=False, ratio=0.2):
     return sublist_1, sublist_2, sublist_3
 
 
-def convert_to_codah_statement(qa_list: list, output_file1: str):
-    print(f'converting qa_list to entailment dataset...')
+def convert_to_codah_statement(input_file: str, output_file1: str):
+    print(f'converting {input_file} to entailment dataset...')
+    tsv_file = pd.read_csv(input_file)
+    qa_list = tsv_file.to_numpy()
     nrow = sum(1 for _ in qa_list)
+    id = 0
     with open(output_file1, 'w') as output_handle1:
         # print("Writing to {} from {}".format(output_file, qa_file))
         for sample in tqdm(qa_list, total=nrow):
-            output_dict = convert_sample_to_entailment(sample)
+            output_dict = convert_sample_to_entailment(sample, id)
             output_handle1.write(json.dumps(output_dict))
             output_handle1.write("\n")
+            id += 1
     print(f'converted statements saved to {output_file1}')
     print()
 
 
 # Convert the QA file json to output dictionary containing premise and hypothesis
-def convert_sample_to_entailment(sample: list):
+def convert_sample_to_entailment(sample: list, id: int):
     question_text = sample[1]
-    choices = sample[2:6]  # left close right open
-    single_qa_dict = {'class': sample[0], 'question': {'stem': sample[1]}, 'answer_key': sample[6]}
+    choices = sample[3:7]  # left close right open
+    single_qa_dict = {'id': id, 'question': {'stem': sample[1]}, 'answer_key': 0}
     choice_list = []
     choice_count = 0
     for choice in choices:
         statement = question_text + ' ' + choice
-        create_output_dict(single_qa_dict, statement, choice_count == sample[6])
+        create_output_dict(single_qa_dict, statement, choice_count == 0)
         choice_list.append({'text': choice, 'label': choice_count})
         choice_count += 1
     single_qa_dict['question']['choices'] = choice_list
@@ -56,9 +60,7 @@ def create_output_dict(input_json: dict, statement: str, label: bool) -> dict:
 
 
 if __name__ == "__main__":
-    full_tsv = pd.read_csv('full_data.tsv', sep='\t')
-    full_list = full_tsv.to_numpy()
-    convert_to_codah_statement(full_list, 'codah.full.jsonl')
+    convert_to_codah_statement('../data/codah/fold_0/train.csv', './data/codah/fold_0/train.jsonl')
     # train, dev, test = split(full_list, shuffle=True, ratio=0.2)
     # convert_to_codah_statement(train, 'train.statement.jsonl')
     # convert_to_codah_statement(dev, 'train.statement.jsonl')
